955c641b8b2faa9a09ec773d4a0f2b6f6adcd911
CarlJorenzGimeno <csgimeno@up.edu.ph>
2023-03-06 12:29:02 +0800

    Test

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const cursor = document.getElementById('coolcursor');
 const navbar = document.querySelector(".navbar");
+const navitem = document.querySelectorAll('.navitem');
+const navlist = [...navitem];
+const preview = document.querySelectorAll('.preview');
+
+
+
 
 document.addEventListener('mousemove', function(event) {
     let body = document.querySelector('body');
@@ -21,8 +27,15 @@ document.addEventListener('mousemove', function(event) {
     },{duration: 300, fill: "forwards"});
 });
 
+
+
 $(".navitem").hover(function(){
-    cursor.style.scale = "1.5";
+        console.log(navlist.indexOf($(this)));
+        preview[0];
+        preview[navlist.indexOf($(this))].classList.add('show');
+        preview[navlist.indexOf($(this))].classList.remove('hidden');
+        cursor.style.scale = "1.5";
+        
     }, function(){
-    cursor.style.scale = "1";
-  });
+        cursor.style.scale = "1";
+    });
